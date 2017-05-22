6ef522796991ec57fc8d2e4095793daaba845b8d
Prelang Builder <builder@prelang.com>
2017-05-22 01:02:39 +0000

    Generated model Questionnaire with migration

---
+++ db/migrate/20170522010239_create_questionnaires.rb
@@ -0,0 +1,12 @@
+class CreateQuestionnaires < ActiveRecord::Migration
+  def change
+    create_table :questionnaires do |t|
+      t.text :question
+      t.string :answer
+      t.text :comment
+      t.references :patient_visit, index: true
+
+      t.timestamps
+    end
+  end
+end
